c9f129dea039821e925dd0f17c293f615cd9331c
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-03-05 10:03:01 +0300

    feat: add yahoo finance data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-import {} from "@stoqey/ib";
-
 import { config } from "dotenv-flow";
+import yahooFinance from "yahoo-finance2";
 config();
+
+const currentMidPrice = async () => {
+  const { regularMarketPrice } = await yahooFinance.quote("ISAC.L");
+
+  if (regularMarketPrice === undefined) {
+    throw new Error(`Invalid regularMarketPrice: ${regularMarketPrice}`);
+  }
+
+  return regularMarketPrice;
+};
+
+const main = async () => {
+  console.log(await currentMidPrice());
+};
+
+main().catch((err) => {
+  process.exitCode = 1;
+  console.error(err);
+});
